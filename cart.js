2a422acc97f27f417a47d09d5037ca8a746e1784
User <KongKunn>
2023-01-06 12:50:03 +0700

    first commit

---
+++ cart.js
@@ -0,0 +1,9 @@
+var namecard=document.querySelector('#namecard');
+var numbercard=document.querySelector('#cardnumber')
+var expMonth=document.querySelector('#expmonth')
+var expYear=document.querySelector('#expyear')
+var cvv=document.querySelector('#cvv')
+var error=document.querySelector('error1');
+function showErrow(input,message){
+    let parent = input.parentElement
+}
---
+++ script.js
@@ -0,0 +1,64 @@
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".navbar");
+var btnopen = document.querySelector('cart2');
+var modal = document.querySelector('modal');
+var btnclose = document.querySelector('xmark');
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+
+  if (window.scrollY > 60) {
+    document.querySelector("#scroll-top").classList.add("active");
+  } else {
+    document.querySelector("#scroll-top").classList.remove("active");
+  }
+};
+loader
+function loader() {
+  document.querySelector(".loader-container").classList.add("fade-out");
+}
+function fadeout() {
+  setInterval(loader, );
+}
+window.onload = fadeout();
+
+let plus = document.querySelectorAll(".plus");
+let minus = document.querySelectorAll(".minus");
+let num = document.querySelectorAll(".num");
+
+for (let i = 0; i < plus.length; i++) {
+  var amount = 0;
+  plus[i].addEventListener("click", () => {
+    amount++;
+    if (amount < 10) {
+      amount = "0" + amount;
+    }
+    num[i].innerText = amount;
+  });
+}
+for (let i = 0; i < minus.length; i++) {
+  var amount = 1;
+  minus[i].addEventListener("click", () => {
+    console.log(minus);
+    if (amount > 1) {
+      amount--;
+      amount = "0" + amount;
+    }
+    if (amount >= 10);
+    {
+      amount = +amount;
+    }
+    if (amount < 10) {
+      amount = "0" + amount;
+    }
+    num[i].innerText = amount;
+  });
+}
+// ------- MODAL----------
+
